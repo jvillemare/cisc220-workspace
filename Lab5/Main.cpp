ecfce948ee45db7eb05204b3ad2d784f56e2aaed
James Villemarette <jvill@udel.edu>
2019-09-24 20:55:32 -0400

    lab 5: stubbing out for future

---
+++ Lab5/Main.cpp
@@ -0,0 +1,28 @@
+/*
+ * Main.cpp
+ *
+ * James Villemarette
+ * (No Partner)
+ *
+ * TA: Lauren Olson
+ *
+ *  Created on: Sep 24, 2019
+ *      Author: jvillemarette
+ *
+ * This file contains functions for lab 5. The functions aren’t necessarily
+ * related in any way other than that they are required for lab 5.
+ */
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main() {
+
+	cout << "Lab 5!" << endl;
+
+	return 0;
+
+}
+
